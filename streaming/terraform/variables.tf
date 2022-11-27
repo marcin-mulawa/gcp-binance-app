812c82ceebc292f052b69c78895bf9730206499c
Marcin Mulawa <marcinamulawa@gmail.com>
2022-11-27 15:42:40 +0100

    Update variables.tf

--- streaming/terraform/variables.tf
+++ streaming/terraform/variables.tf
@@ -1,36 +1,37 @@
 variable "project_id" {
-  description = "The project ID "
-  type = string
+  description = "The project name"
+  type        = string
+  default     = "binance-app-gcp"
 }
 
 variable "topic_name" {
   description = "The name of the topic"
-  type = string
-  default = "binance"
+  type        = string
+  default     = "binance"
 }
 
 variable "subscription_name" {
   description = "The name of the subscription"
-  type = string
-  default = "binance-sub"
+  type        = string
+  default     = "binance-sub"
 }
 
 variable "dataset_name" {
   description = "The name of the dataset"
-  type = string
-  default = "binance"
+  type        = string
+  default     = "binance"
 }
 
 variable "dataset_location" {
   description = "The location of the dataset"
-  type = string
-  default = "eu"
+  type        = string
+  default     = "eu"
 }
 
 variable "table_name" {
   description = "The name of the table"
-  type = string
-  default = "binance_klines"
+  type        = string
+  default     = "binance_klines"
 }
 
 variable "table_schema" {
